5380e6f7cdf00800834c433f4b1e574d9dbe6375
Rafał Miler <rafalmiler77@gmail.com>
2017-02-13 17:51:46 +0100

    input form created

--- src/App.js
+++ src/App.js
@@ -3,16 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(){
+    super()
+
+this.state = {
+      name: ''
+}
+this.handleSubmit = event => {
+      event.preventDefault()
+  this.setState({
+    name: this.state.name
+  })
+}
+  }
+    render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <form>
+        <input onSubmit={this.handleSubmit}
+          value={this.state.name}
+          type="text"
+          onChange={
+            event => this.setState({
+              name: event.target.value
+            })
+          }
+        />
+          <button type="submit">Go!</button>
+        </form>
+        <h3>{this.state.name}</h3>
       </div>
     );
   }
