5e83887dc15b1a79eda6a0be190a5694204239c8
Rafał Miler <rafalmiler77@gmail.com>
2017-02-25 12:47:06 +0100

    React-router installed, changes made to make router work

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import  App  from './App/App';
+import  mainView  from './main-view/mainView';
 import store from './store';
 import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
-  </Provider>
-  ,
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={App}/>
+      </Route >
+      <Route path="/mainView" component={mainView} />
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
